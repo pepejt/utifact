307da8d467a3afd659f67c695cad938fecd378aa
Fernando-8a <166525755+Fernando-8a@users.noreply.github.com>
2025-01-13 13:42:52 -0500

    clientes ingreso

---
+++ app/src/main/java/ec/uti/edu/utifact/MainActivity.kt
@@ -0,0 +1,26 @@
+package ec.uti.edu.utifact
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onIngresoClick(view: View) {
+        startActivity(Intent(this, ingreso::class.java))
+    }
+}
